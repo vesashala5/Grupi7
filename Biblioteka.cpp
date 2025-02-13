b1e2efb2bb14c6eab9a2eb93dd989760e59dc44c
almajashari <alma.jashari2@student.uni-pr.edu>
2025-02-13 14:49:14 +0100

    Structure added

---
+++ Biblioteka.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// Struktura per te perfaqesuar nje liber
+struct Liber {
+    string titulli;
+    string autori;
+    bool ne_sirtar;
+};
