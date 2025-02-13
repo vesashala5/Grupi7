e9f4b1f02efa4c5a73a09b32a974e95244405de8
almajashari <alma.jashari2@student.uni-pr.edu>
2025-02-13 14:50:11 +0100

    Function to show all books

--- Biblioteka.cpp
+++ Biblioteka.cpp
@@ -20,3 +20,15 @@ void shtoLiber(vector<Liber>& biblioteka, string titulli, string autori, bool ne
     biblioteka.push_back({titulli, autori, ne_sirtar});
     cout << "Libri \"" << titulli << "\" u shtua me sukses!" << endl;
 }
+// Funksioni per te shfaqur te gjithe librat ne biblioteke
+void shfaqBiblioteken(const vector<Liber>& biblioteka) {
+    if (biblioteka.empty()) {
+        cout << "Biblioteka eshte bosh!" << endl;
+        return;
+    }
+    cout << "\nLista e librave ne biblioteke:" << endl;
+    for (const auto& liber : biblioteka) {
+        cout << "Titulli: " << liber.titulli << ", Autori: " << liber.autori 
+             << ", Statusi: " << (liber.ne_sirtar ? "Ne sirtar" : "Mungon") << endl;
+    }
+}
