01d3031ba73f7d62c1162ffd489893e0a0b53f16
almajashari <alma.jashari2@student.uni-pr.edu>
2025-02-13 14:49:43 +0100

    Function to add a book

--- Biblioteka.cpp
+++ Biblioteka.cpp
@@ -9,3 +9,14 @@ struct Liber {
     string autori;
     bool ne_sirtar;
 };
+// Funksioni per te shtuar nje liber ne biblioteke
+void shtoLiber(vector<Liber>& biblioteka, string titulli, string autori, bool ne_sirtar) {
+    for (const auto& liber : biblioteka) {
+        if (liber.titulli == titulli) {
+            cout << "Ky liber tashme ekziston ne biblioteke!" << endl;
+            return;
+        }
+    }
+    biblioteka.push_back({titulli, autori, ne_sirtar});
+    cout << "Libri \"" << titulli << "\" u shtua me sukses!" << endl;
+}
